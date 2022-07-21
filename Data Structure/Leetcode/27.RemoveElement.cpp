3788d5a6190001dca6d9d45942a4dd7e69b8e1ca
cyndereN <50982416+cyndereN@users.noreply.github.com>
2022-07-22 01:23:34 +0800

    Update lc27

--- Data Structure/Leetcode/27.RemoveElement.cpp
+++ Data Structure/Leetcode/27.RemoveElement.cpp
@@ -6,6 +6,38 @@
 
 // 数组的元素在内存地址中是连续的，不能单独删除数组中的某个元素，只能覆盖。
 // 1.暴力
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int size = nums.size();
+        for (int i = 0; i < size; i++) {
+            if (nums[i] == val) { // 发现需要移除的元素，就将数组集体向前移动一位
+                for (int j = i + 1; j < size; j++) {
+                    nums[j - 1] = nums[j];
+                }
+                i--; // 因为下标i以后的数值都向前移动了一位，所以i也向前移动一位
+                size--; // 此时数组的大小-1
+            }
+        }
+        return size;
 
+    }
+};
+
+// 时间复杂度：O(n^2)
+// 空间复杂度：O(1)
 
 // 2.双指针
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.size(); fastIndex++){
+            if(val != nums[fastIndex]){
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+};

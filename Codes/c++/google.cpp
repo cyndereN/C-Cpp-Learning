32f9e34a9a0d9641ecea2831ffdbab26fba05a0e
cyndereN <50982416+cyndereN@users.noreply.github.com>
2022-11-20 02:46:26 +0000

    [Mod] Update Map erasing

--- Codes/c++/google.cpp
+++ Codes/c++/google.cpp
@@ -86,3 +86,45 @@ int solution(vector<int> A) {
   
   return value1 - value2;
 }
+/*
+报错原因：采用erase移除迭代器后，迭代器的值变为-572662307，无法作为迭代器继续运算。
+详细：当程序执行到 stu_map.erase(itor) 时，满足条件的第一个元素被删除，从而导致 itor 指针被删除，使其不指向任何元素。针对该问题我们首先想到的就是在删除指针之前，对其进行备份，C++ 将这个临时变量直接建立在 erase 实现里。
+因此将stu_map.erase(itor)改为stu_map.erase(itor--)即可
+*/
+#include <map>
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	// 初始化变量
+	map<int, char> stu_map;
+ 
+	// 变量的赋值
+	stu_map[10010] = 'm';
+	stu_map[10011] = 'f';
+	stu_map[10012] = 'f';
+	stu_map[10013] = 'm';
+	stu_map[10014] = 'f';
+	stu_map[10015] = 'm';
+ 
+	// 循环删除value为“f”的元素
+	map<int, char>::iterator itor;
+	for (itor = stu_map.begin(); itor != stu_map.end(); itor++)
+	{
+		if (itor->second == 'f')
+		{
+			// 移除元素
+			stu_map.erase(itor--);
+		}
+		else 
+		{
+			int stu_num = itor->first;
+			char stu_sex = itor->second;
+ 
+			cout << "key = " << stu_num << ", value = " << stu_sex << endl;
+		}
+	}
+ 
+	return 0;
+}
